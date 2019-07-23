3db4c665a25fa57e12c9bf8b5de902ca4c739bb1
Abey Onalaja <abideenonalaja@Abideens-Mac-Pro.local>
2019-07-24 00:12:42 +0100

    fix typos

--- src/snake.rs
+++ src/snake.rs
@@ -1,5 +1,25 @@
-use std::collections::LinkedList;
-use piston_window::{Context, G2d};
 use piston_window::types::Color;
+use piston_window::{Context, G2d};
+use std::collections::LinkedList;
 
 use draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::UP => Direction::Down,
+            Direction::Down => Direction::UP,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
